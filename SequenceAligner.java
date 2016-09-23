ae74f145ae2917501df22f90d1f72bd399a803dc
imyjimmy <imyjimmy@gmail.com>
2016-09-23 18:06:40 -0400

    added direction matrix and enums for direction matrix.

--- SequenceAligner.java
+++ SequenceAligner.java
@@ -19,6 +19,10 @@ public class SequenceAligner{
     public int mismatchPenalty = -1;
 
     public String seq_type;
+
+    public enum Direction {
+        STRT, LEFT, TOP, DG, LD, TD, LT, ALL;
+    }
     
     /*
      Todo: as the user for scoring parameters. As noted in the assignment.
@@ -34,13 +38,15 @@ public class SequenceAligner{
         if (keys.length == 2) {
             String seq1 = s.get(keys[0]);
             String seq2 = s.get(keys[1]);
-            int[][] matrix = seq_aligner.createMatrix(seq1, seq2);
 
-            seq_aligner.printMatrix(matrix, seq1, seq2);
-            seq_aligner.initializeMatrix(matrix);
+            int[][] matrix = seq_aligner.createMatrix(seq1, seq2);
+            Direction[][] cell_origin_matrix = new Direction[seq1.length()+1][seq2.length()+1];
+
+            seq_aligner.printMatrix(matrix, cell_origin_matrix, seq1, seq2);
+            seq_aligner.initializeMatrix(matrix, cell_origin_matrix);
 
             //Begin the algorithm.
-            seq_aligner.needlemanWunsch(matrix, seq1, seq2);
+            seq_aligner.needlemanWunsch(matrix, cell_origin_matrix, seq1, seq2);
 
         } else { //multiple pair alignment. todo.
             //nothing for now.
@@ -100,7 +106,7 @@ public class SequenceAligner{
         return new int[seq1.length()+1][seq2.length()+1];
     }
 
-    public void initializeMatrix(int[][] matrix) {
+    public void initializeMatrix(int[][] matrix, Direction[][] cell_origin) {
         //-5 = opening penalty
         //-1 = extension penalty
         System.out.println("public void initializeMatrix(int[][] matrix) ");
@@ -113,53 +119,77 @@ public class SequenceAligner{
                         matrix[i][j] = -5+(-1*(j-1));
                     }
                 } 
-            }
-            else { //i = 1, 2, etc.
+            } else { //i = 1, 2, etc.
                 matrix[i][0] = -5+(-1*(i-1));
             }
         }
 
-        this.printMatrix(matrix, "", "");
+        for (int i = 0; i < cell_origin.length; i++) {
+            if (i == 0) {
+                for (int j = 0; j < cell_origin[i].length; j++) {
+                    if (j == 0) {
+                        cell_origin[i][j] = Direction.STRT;
+                    } else {
+                        cell_origin[i][j] = Direction.LEFT;
+                    }
+                }
+            } else {
+                cell_origin[i][0] = Direction.TOP;
+            }
+        }
+
+        this.printMatrix(matrix, cell_origin, "", "");
      }
 
     /* The global gap alignment algorithm begins here.
     */
-    public void needlemanWunsch(int[][] matrix, String seq1, String seq2) {
-        System.out.println("public void needlemanWunsch(int[][] matrix, String seq1, String seq2)");
-        this.printMatrix(matrix, seq1, seq2);
+    public void needlemanWunsch(int[][] matrix, Direction[][] cell_origin, String seq1, String seq2) {
+        System.out.println("public void needlemanWunsch(int[][] matrix, Direction[][] cell_origin, String seq1, String seq2)");
+        this.printMatrix(matrix, cell_origin, seq1, seq2);
         //important that both i and j start with 1, 
         //because matrix 1st row and column has already been initialized!!
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
-                matrix[i][j] = this.findMaxScore(matrix, i, j, seq1, seq2);
+                matrix[i][j] = this.findMaxScore(matrix, cell_origin, i, j, seq1, seq2);
             }
         }
 
         System.out.println("Ran the al-gore-rhythm, here is the matrix:");
 
-        this.printMatrix(matrix, seq1, seq2);
+        this.printMatrix(matrix, cell_origin, seq1, seq2);
     }
 
-    public int findMaxScore(int[][] matrix, int i, int j, String seq1, String seq2) {
+    public int findMaxScore(int[][] matrix, Direction[][] cell_origin, int i, int j, String seq1, String seq2) {
         System.out.println("public void findMaxScore(int[][] matrix, int i, int j)");
-        this.printMatrix(matrix, seq1, seq2);
+        this.printMatrix(matrix, cell_origin, seq1, seq2);
 
         //from the left
         int left = matrix[i][j-1] + this.decideGapPenalty(matrix, i, j, i, j-1);
-        int max = left; //max score so far.
+        int max = left; //max score so far. 
+        cell_origin[i][j] = Direction.LEFT;
 
         //from top therfore decrement i.
         int top = matrix[i-1][j] + this.decideGapPenalty(matrix, i, j, i-1, j);
         
         if (top > max) {
             max = top;
+            cell_origin[i][j] = Direction.TOP;
         } else if (top == left) {
-          //tie score, what to do? @todo
+          //tie score, what to do? max is still same value, but cell_origin needs to take note
+            cell_origin[i][j] = Direction.LT;
         }
 
+        //DG, LD, TD, LT, ALL;
         int diag = matrix[i-1][j-1] + this.matchOrMismatch(i-1, j-1, seq1, seq2);
         if (diag > max) {
             max = diag;
+            cell_origin[i][j] = Direction.DG;
+        } else if (diag == max && cell_origin[i][j] == Direction.LEFT) {
+            cell_origin[i][j] = Direction.LD;
+        } else if (diag == max && cell_origin[i][j] == Direction.TOP) {
+            cell_origin[i][j] = Direction.TD;
+        } else if (diag == max && cell_origin[i][j] == Direction.LT) {
+            cell_origin[i][j] = Direction.ALL;
         }
 
         System.out.println("left: " + left + " top: " + top + " diag: " + diag + " max: " + max);
@@ -194,50 +224,42 @@ public class SequenceAligner{
             }
         }
     }
-/*
-    THIS NEEDS WORK RIGHT NOW.
-    4   10
-    4   5
-
-*/
-    // public String cellOrigin(int[][] matrix, int i, int j, String seq1, String seq2) {
-    //     String toReturn;
-    //     if () {
-    //         return "LEFT";
-    //     } else if () {
-    //         return "TOP";
-    //     } else if (seq1.charAt(i) == seq2.charAt(j) && matrix[i][j] - matrix[i-1][j-1] == matchScore) {
-    //         //assume the match score will be awarded from the diagonal when there's a match. 
-    //         //(highest possible value)
-    //         toReturn = "DIAG";
-    //     } else if (seq1.charAt(i) != seq2.charAt(j) && matrix[i][j] - matrix[i-1][j-1] == mismatchPenalty) {
-    //         //assume that mismatch penalty will be higher priority than gaps. 
-    //         toReturn = "DIAG";
-    //     }
-    // }
 
     public int matchOrMismatch(int i, int j, String seq1, String seq2) {
         // System.out.println("sequence type: " + seq_type);
-        if (seq1.charAt(i) == seq2.charAt(j)) {
-            return matchScore;
-        } else {
-            return mismatchPenalty;
+        if (seq_type.equals("N") || seq_type.equals("n")) { 
+            if (seq1.charAt(i) == seq2.charAt(j)) {
+                return matchScore;
+            } else {
+                return mismatchPenalty;
+            }
+        } else { //protein comparison. use that pam matrix.
+            return 0;
         }
     }
 
     public String getAlignment(int[][] matrix) {
         ArrayList<String> alignment = new ArrayList<String>();
         String[] directions = new String[matrix.length + matrix[0].length];
-        //from the bottom left, call origin(matrix, i, j)
+        //from the bottom left, call cellOrigin(matrix, i, j)
         return "hey ma!";
     }
 
-    public void printMatrix(int[][] matrix, String seq1, String seq2) {
+    public void printMatrix(int[][] matrix, Direction[][] cell_origin, String seq1, String seq2) {
+        System.out.println("here's the alignment matrix:");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + "\t");
             }   
             System.out.println();
         }
-     }
+
+        System.out.println("here's the cell origin matrix"); 
+        for (int i = 0; i < cell_origin.length; i++) {
+            for (int j = 0; j < cell_origin[i].length; j++) {
+                System.out.print(cell_origin[i][j] + "\t");
+            }   
+            System.out.println();
+        }
+    }
 }
